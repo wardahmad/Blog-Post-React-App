7f57cdd3a54b6d4c6fe27f5f55b9b54a8d36c74b
wardahmad <whitelight43@hotmail.com>
2020-01-17 20:35:46 +0300

    Updates

---
+++ precache-manifest.79b32c3c0d2b1c10650167c4dbf0fd06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "753f8a7ad880d15924e2515b7e0b379c",
+    "url": "/wardahmad/Blog-Post-React-App/index.html"
+  },
+  {
+    "revision": "59d68d252fc6aebde01f",
+    "url": "/wardahmad/Blog-Post-React-App/static/css/main.9679c6b3.chunk.css"
+  },
+  {
+    "revision": "de4387dc762b36453d54",
+    "url": "/wardahmad/Blog-Post-React-App/static/js/2.e6f8461b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/wardahmad/Blog-Post-React-App/static/js/2.e6f8461b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "59d68d252fc6aebde01f",
+    "url": "/wardahmad/Blog-Post-React-App/static/js/main.23e52e34.chunk.js"
+  },
+  {
+    "revision": "d6fc166cd0606cc8c837",
+    "url": "/wardahmad/Blog-Post-React-App/static/js/runtime-main.2d782823.js"
+  }
+]);
